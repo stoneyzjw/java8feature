e0e65bfa97223f1ce2f4ee2460dcc90b6c8510c4
stoney <stoney@ol65.longyan.fjgs.tax>
2014-11-04 10:04:35 +0800

    测试内部类，AccessingLocalVariables访问本地变量。

---
+++ src/main/java/tax/fjgs/longyan/java8feature/chapter02/AccessingLocalVariables.java
@@ -0,0 +1,21 @@
+package tax.fjgs.longyan.java8feature.chapter02;
+
+public class AccessingLocalVariables
+{
+	public static void main(String[] args)
+	{
+		int x = 100;
+		final int y = 200;
+		class LocalInner
+		{
+			void print()
+			{
+				System.out.println("x = " + x);
+				System.out.println("y = " + y);
+			}
+		}
+		// x = 100; x must effectively final
+		LocalInner li = new LocalInner();
+		li.print();
+	}
+}
