83c231f0b567e18cb942f3a89fee0d191627d5c6
stoney <stoney@ol65.longyan.fjgs.tax>
2014-11-04 16:20:07 +0800

    第六章 增加CallableTask CallableTaskTest类。

---
+++ src/main/java/tax/fjgs/longyan/java8feature/chapter06/CallableTaskTest.java
@@ -0,0 +1,31 @@
+package tax.fjgs.longyan.java8feature.chapter06;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+
+public class CallableTaskTest
+{
+	public static void main(String[] args)
+	{
+		ExecutorService exec = Executors.newFixedThreadPool(3);
+		CallableTask task = new CallableTask(1, 3);
+		Future<Integer> submitTask = exec.submit(task);
+		try
+		{
+			Integer result = submitTask.get();
+			System.out.println("任务总睡眠时间：" + result + " 秒。");
+		}
+		catch (ExecutionException e)
+		{
+			System.out.println("Error in executing the task.");
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Task execution has been interrupted.");
+		}
+		exec.shutdown();
+	}
+}
+
