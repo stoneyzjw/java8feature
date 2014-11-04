4611e12ffc80449ba8561fff9c23a2bbbb378ed0
stoney <stoney@ol65.longyan.fjgs.tax>
2014-11-04 16:25:53 +0800

    第六章 在CallableTaskTest中增加一个CallableTask实例

--- src/main/java/tax/fjgs/longyan/java8feature/chapter06/CallableTaskTest.java
+++ src/main/java/tax/fjgs/longyan/java8feature/chapter06/CallableTaskTest.java
@@ -11,11 +11,15 @@ public class CallableTaskTest
 	{
 		ExecutorService exec = Executors.newFixedThreadPool(3);
 		CallableTask task = new CallableTask(1, 3);
+		CallableTask taskTemp = new CallableTask(2, 3);
 		Future<Integer> submitTask = exec.submit(task);
+		Future<Integer> tempTask = exec.submit(taskTemp);
 		try
 		{
 			Integer result = submitTask.get();
 			System.out.println("任务总睡眠时间：" + result + " 秒。");
+			Integer result2 = tempTask.get();
+			System.out.println("任务总睡眠时间：" + result2 + " 秒。");
 		}
 		catch (ExecutionException e)
 		{
